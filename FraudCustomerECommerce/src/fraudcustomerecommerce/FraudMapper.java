a79642f8e6256de6147904d67c24d500b2c00f72
drwhoami <amann12997@gmail.com>
2018-06-12 10:15:06 +0530

    first commit

---
+++ FraudCustomerECommerce/src/fraudcustomerecommerce/FraudMapper.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fraudcustomerecommerce;
+
+import java.io.IOException;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+/**
+ *
+ * @author logan
+ */
+public class FraudMapper extends Mapper<LongWritable, Text, Text, FraudWritable> {
+    public void map(LongWritable key, Text value, Context con)throws IOException, InterruptedException
+    {
+        String[] fields = value.toString().split(",");
+        FraudWritable details = new FraudWritable();
+        details.set(fields[1],fields[5],fields[6],fields[7]);
+        con.write(new Text(fields[0]),details);
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # hadoop-MapReduce
+#Template Reduce Side Data Join
+# fraud-customer-detection
